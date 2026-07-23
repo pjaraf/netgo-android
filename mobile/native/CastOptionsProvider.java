7c634e83c863c0bd43527de33f0d2f85cf50dff9
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-07-23 10:55:08 -0400

    Add files via upload

---
+++ mobile/native/CastOptionsProvider.java
@@ -0,0 +1,36 @@
+package com.netgo.mobile;
+
+import android.content.Context;
+
+import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.cast.framework.CastOptions;
+import com.google.android.gms.cast.framework.OptionsProvider;
+import com.google.android.gms.cast.framework.SessionProvider;
+import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.NotificationOptions;
+
+import java.util.List;
+
+/**
+ * Required configuration class for the Google Cast SDK. Uses Google's
+ * default receiver app (works for casting direct video URLs like ours,
+ * no custom receiver app needed).
+ */
+public class CastOptionsProvider implements OptionsProvider {
+    @Override
+    public CastOptions getCastOptions(Context context) {
+        NotificationOptions notificationOptions = new NotificationOptions.Builder().build();
+        CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
+                .setNotificationOptions(notificationOptions)
+                .build();
+        return new CastOptions.Builder()
+                .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+                .setCastMediaOptions(mediaOptions)
+                .build();
+    }
+
+    @Override
+    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+        return null;
+    }
+}
