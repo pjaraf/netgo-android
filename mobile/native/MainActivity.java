118a121ebdbb6358d99e9ffb8d8addab3a606d0e
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-07-23 00:59:45 -0400

    Enable mixed content mode in WebView
    Allow mixed content in WebView settings for MainActivity.

--- mobile/native/MainActivity.java
+++ mobile/native/MainActivity.java
@@ -1,6 +1,7 @@
 package com.netgo.mobile;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +9,6 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(VlcPlayerPlugin.class);
         super.onCreate(savedInstanceState);
+        getBridge().getWebView().getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
     }
 }
